23f826f19b4af135f5bed4a77c1e5bb5720015be
Felipe <felipe.wd.dias@outlook.com>
2022-04-28 19:55:39 -0300

    Modificações no Projeto, apredendo Elif e While

--- Python 3 parte 1/02_Lidando com entrada do usuário/Projeto/adivinhacao.py
+++ Python 3 parte 1/02_Lidando com entrada do usuário/Projeto/adivinhacao.py
@@ -4,17 +4,32 @@ print("*********************************")
 
 numero_secreto = 29
 
-chute = input("Digite um número: ")
-print("Você digitou ", chute)
+rodada = 1
+numero_de_tentativas = 5
 
-# A função input sempre retorna um texto (string). Sendo assim, é necessário converter o valor retornado em inteiro,
-# para que a comparação com outro inteiro, no caso o numero_secreto, seja possível:
-# Realizado a criação da variável "digito" para que receba o valor da conversão.
-digito = int(chute)
+while(rodada <= numero_de_tentativas):
 
-if(digito == numero_secreto):
-    print("Você acertou! :)")
-else:
-    print("Você errou, :(")
+    print("Tentativa", rodada, "de", numero_de_tentativas)
+    chute = input("Digite um número: ")
+    print("Você digitou ", chute)
+    # A função input sempre retorna um texto (string). Sendo assim, é necessário converter o valor retornado em inteiro,
+    # para que a comparação com outro inteiro, no caso o numero_secreto, seja possível:
+    # Realizado a criação da variável "digito" para que receba o valor da conversão.
+    digito = int(chute)
+
+    acertou = digito == numero_secreto
+    maior   = digito > numero_secreto
+    menor   = digito < numero_secreto
+    stop    = digito == numero_secreto
+
+    if(acertou):
+        print("Você acertou! :)")
+    else:
+        if(maior):
+            print("Você errou! O seu chute foi maior do que o número secreto.")
+        elif(menor):
+            print("Você errou! O seu chute foi menor do que o número secreto.")
+
+    rodada = rodada + 1
 
 print("Fim do jogo")
