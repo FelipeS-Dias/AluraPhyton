b602094b1558937cfb90e3dbe32b1c3492357d52
Felipe <felipe.wd.dias@outlook.com>
2022-04-28 20:58:11 -0300

    Formatando a identação do código

--- Python 3 parte 1/02_Lidando com entrada do usuário/Projeto/adivinhacao.py
+++ Python 3 parte 1/02_Lidando com entrada do usuário/Projeto/adivinhacao.py
@@ -2,34 +2,32 @@ print("*********************************")
 print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
-numero_secreto = 29
+numero_secreto = 55
 
+numero_de_tentativas = 3
 rodada = 1
-numero_de_tentativas = 5
 
 while(rodada <= numero_de_tentativas):
-
     print("Tentativa {} de {}" .format(rodada, numero_de_tentativas))
-    chute = input("Digite um número: ")
-    print("Você digitou ", chute)
+
+    chute_str = input("Digite um número: ")
+    print("Você digitou", chute_str)
     # A função input sempre retorna um texto (string). Sendo assim, é necessário converter o valor retornado em inteiro,
     # para que a comparação com outro inteiro, no caso o numero_secreto, seja possível:
     # Realizado a criação da variável "digito" para que receba o valor da conversão.
-    digito = int(chute)
+    chute = int(chute_str)
 
-    acertou = digito == numero_secreto
-    maior   = digito > numero_secreto
-    menor   = digito < numero_secreto
-    stop    = digito == numero_secreto
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
     if(acertou):
-        print("Você acertou! :)")
+        print("Voce acertou! Parabéns!!")
     else:
         if(maior):
-            print("Você errou! O seu chute foi maior do que o número secreto.")
+            print("Voce errou! O chute foi maior do que o número secreto.")
         elif(menor):
-            print("Você errou! O seu chute foi menor do que o número secreto.")
-
+            print("Você errou! O Chute foi menor do que o número secreto.")
     rodada = rodada + 1
 
 print("Fim do jogo")
