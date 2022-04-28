69baba959725900af8e04b5a8a780d134fc6b67c
Felipe <felipe.wd.dias@outlook.com>
2022-04-27 21:54:07 -0300

    Alterações

---
+++ Python 3 parte 1/02_Lidando com entrada do usuário/Atividades/atividade_01.py
@@ -0,0 +1,22 @@
+print("____________")
+print("Atividade 01")
+print("____________")
+
+minha_idade = 26
+idade_namorado = 25
+
+if(minha_idade == idade_namorado):
+    print('Temos idades iguais')
+else:
+    print('Temos idades diferentes')
+
+# O Exercicio era para identificar a falha de digitação no código,
+#
+# O problema era que para indicar o início do bloco if, é necessário utilizar dois pontos (:).
+#
+# minha_idade = 26
+# idade_namorado = 25
+# if(minha_idade == idade_namorado)
+#     print('temos idades iguais')
+# else:
+#     print('temos idades diferentes')
