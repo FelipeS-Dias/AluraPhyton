6e181fd2616f27872780e0d0830e5505527a17a5
Felipe <felipe.wd.dias@outlook.com>
2022-04-27 21:52:37 -0300

    Adicionando os primeiros arquivos gerados na aula de Python

---
+++ Python 3 parte 1/02_Lidando com entrada do usuário/Projeto/adivinhacao.py
@@ -0,0 +1,20 @@
+print("*********************************")
+print("Bem vindo ao jogo de Adivinhação!")
+print("*********************************")
+
+numero_secreto = 29
+
+chute = input("Digite um número: ")
+print("Você digitou ", chute)
+
+# A função input sempre retorna um texto (string). Sendo assim, é necessário converter o valor retornado em inteiro,
+# para que a comparação com outro inteiro, no caso o numero_secreto, seja possível:
+# Realizado a criação da variável "digito" para que receba o valor da conversão
+digito = int(chute)
+
+if(digito == numero_secreto):
+    print("Você acertou! :)")
+else:
+    print("Você errou, :(")
+
+print("Fim do jogo")
