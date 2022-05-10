3ddeee5dfabe97b74f7dce0c6122e2fa2976b3e8
Felipe <felipe.wd.dias@outlook.com>
2022-05-09 21:40:31 -0300

    Adicionando modificações, verificação de nível e pontuação

--- Python 3 parte 1/02_Lidando com entrada do usuário/Projeto/adivinhacao.py
+++ Python 3 parte 1/02_Lidando com entrada do usuário/Projeto/adivinhacao.py
@@ -6,9 +6,24 @@ print("*********************************")
 
 numero_secreto = random.randrange(1,101)
 
-numero_de_tentativas = 3
+numero_de_tentativas = 0
 
-print(numero_secreto)
+pontos = 1000
+
+
+print("Escolha o nível de dificuldade", numero_secreto)
+print("(1)Fácil (2)Médio (3)Difícil")
+
+nivel = int(input("Digite o número da dificuldade que deseja jogar: "))
+
+if (nivel == 1):
+    numero_de_tentativas = 15
+elif (nivel == 2):
+    numero_de_tentativas = 10
+elif (nivel == 3):
+    numero_de_tentativas = 5
+else:
+    print("Favor digitar um número válido 1, 2 ou 3")
 
 for rodada in range(1, numero_de_tentativas + 1):
     print("Tentativa {} de {}" .format(rodada, numero_de_tentativas))
@@ -29,12 +44,14 @@ for rodada in range(1, numero_de_tentativas + 1):
     menor = chute < numero_secreto
 
     if(acertou):
-        print("Voce acertou! Parabéns!!")
+        print("Voce acertou! Parabéns!! O seu total de pontos foi {}!" .format(pontos))
         break
     else:
         if(maior):
             print("Voce errou! O chute foi maior do que o número secreto.")
         elif(menor):
             print("Você errou! O Chute foi menor do que o número secreto.")
+            pontos_perdidos = abs(numero_secreto - chute)
+            pontos = pontos - pontos_perdidos
 
 print("Fim do jogo")
